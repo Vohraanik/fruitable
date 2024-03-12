48b84ec950ecdb0475ad0688e2e58a234c486be2
Vohraanik <153048777+Vohraanik@users.noreply.github.com>
2024-03-12 12:13:22 +0530

    fruitable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Headers from "./user/component/Header/Header";
+import Footer from "./user/component/Footer/Footer";
+import Home from "./user/container/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Headers />
+<Home />
+<Footer />
+</>
   );
 }
 
